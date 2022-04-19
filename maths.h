85cf3c163a76e3e0367e2489093ba652e1c9f8df
MangoSister <yyp0502@gmail.com>
2022-04-19 16:29:03 -0700

    Morton code utility.

--- maths.h
+++ maths.h
@@ -395,3 +395,72 @@ inline float delta_angle(float alpha, float beta)
     float distance = phi > pi ? two_pi - phi : phi;
     return distance;
 }
+
+// https://fgiesen.wordpress.com/2009/12/13/decoding-morton-codes/
+// Also pbrt hair doc
+// TODO: as of Haswell, the PEXT instruction could do all this in a
+// single instruction.
+
+// "Insert" a 0 bit after each of the 16 low bits of x
+constexpr uint32_t part_1_by_1(uint32_t x)
+{
+    x &= 0x0000ffff;                 // x = ---- ---- ---- ---- fedc ba98 7654 3210
+    x = (x ^ (x << 8)) & 0x00ff00ff; // x = ---- ---- fedc ba98 ---- ---- 7654 3210
+    x = (x ^ (x << 4)) & 0x0f0f0f0f; // x = ---- fedc ---- ba98 ---- 7654 ---- 3210
+    x = (x ^ (x << 2)) & 0x33333333; // x = --fe --dc --ba --98 --76 --54 --32 --10
+    x = (x ^ (x << 1)) & 0x55555555; // x = -f-e -d-c -b-a -9-8 -7-6 -5-4 -3-2 -1-0
+    return x;
+}
+
+// "Insert" two 0 bits after each of the 10 low bits of x
+constexpr uint32_t part_1_by_2(uint32_t x)
+{
+    x &= 0x000003ff;                  // x = ---- ---- ---- ---- ---- --98 7654 3210
+    x = (x ^ (x << 16)) & 0xff0000ff; // x = ---- --98 ---- ---- ---- ---- 7654 3210
+    x = (x ^ (x << 8)) & 0x0300f00f;  // x = ---- --98 ---- ---- 7654 ---- ---- 3210
+    x = (x ^ (x << 4)) & 0x030c30c3;  // x = ---- --98 ---- 76-- --54 ---- 32-- --10
+    x = (x ^ (x << 2)) & 0x09249249;  // x = ---- 9--8 --7- -6-- 5--4 --3- -2-- 1--0
+    return x;
+}
+
+// Inverse of part_1_by_1 - "delete" all odd-indexed bits
+constexpr uint32_t compact_1_by_1(uint32_t x)
+{
+    x &= 0x55555555;                 // x = -f-e -d-c -b-a -9-8 -7-6 -5-4 -3-2 -1-0
+    x = (x ^ (x >> 1)) & 0x33333333; // x = --fe --dc --ba --98 --76 --54 --32 --10
+    x = (x ^ (x >> 2)) & 0x0f0f0f0f; // x = ---- fedc ---- ba98 ---- 7654 ---- 3210
+    x = (x ^ (x >> 4)) & 0x00ff00ff; // x = ---- ---- fedc ba98 ---- ---- 7654 3210
+    x = (x ^ (x >> 8)) & 0x0000ffff; // x = ---- ---- ---- ---- fedc ba98 7654 3210
+    return x;
+}
+
+// Inverse of part_1_by_2 - "delete" all bits not at positions divisible by 3
+constexpr uint32_t compact_1_by_2(uint32_t x)
+{
+    x &= 0x09249249;                  // x = ---- 9--8 --7- -6-- 5--4 --3- -2-- 1--0
+    x = (x ^ (x >> 2)) & 0x030c30c3;  // x = ---- --98 ---- 76-- --54 ---- 32-- --10
+    x = (x ^ (x >> 4)) & 0x0300f00f;  // x = ---- --98 ---- ---- 7654 ---- ---- 3210
+    x = (x ^ (x >> 8)) & 0xff0000ff;  // x = ---- --98 ---- ---- ---- ---- 7654 3210
+    x = (x ^ (x >> 16)) & 0x000003ff; // x = ---- ---- ---- ---- ---- --98 7654 3210
+    return x;
+}
+
+constexpr uint32_t encode_morton_2(uint32_t x, uint32_t y) { return (part_1_by_1(y) << 1) + part_1_by_1(x); }
+
+constexpr uint32_t encode_morton_3(uint32_t x, uint32_t y, uint32_t z)
+{
+    return (part_1_by_2(z) << 2) + (part_1_by_2(y) << 1) + part_1_by_2(x);
+}
+
+constexpr void decode_morton_2(uint32_t code, uint32_t &x, uint32_t &y)
+{
+    x = compact_1_by_1(code >> 0);
+    y = compact_1_by_1(code >> 1);
+}
+
+inline void decode_morton_3(uint32_t code, uint32_t &x, uint32_t &y, uint32_t &z)
+{
+    x = compact_1_by_2(code >> 0);
+    y = compact_1_by_2(code >> 1);
+    z = compact_1_by_2(code >> 2);
+}
