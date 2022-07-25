7c75da91f2d8e91912e2a6850910cd3856452319
MangoSister <yyp0502@gmail.com>
2022-07-25 01:45:52 -0700

    Added hash11u and hash12u.

--- hash.h
+++ hash.h
@@ -10,6 +10,26 @@ inline arr<N> convert_u32_f01(arru<N> u32)
     return u32.cast<float>() / float(0xffffffffu);
 }
 
+inline uint32_t hash11u(uint32_t p)
+{
+    // xxhash32
+    const uint32_t PRIME32_2 = 2246822519U, PRIME32_3 = 3266489917U;
+    const uint32_t PRIME32_4 = 668265263U, PRIME32_5 = 374761393U;
+    uint32_t h32 = p + PRIME32_5;
+    h32 = PRIME32_4 * ((h32 << 17) | (h32 >> (32 - 17)));
+    h32 = PRIME32_2 * (h32 ^ (h32 >> 15));
+    h32 = PRIME32_3 * (h32 ^ (h32 >> 13));
+    return h32 ^ (h32 >> 16);
+}
+
+inline arr2u hash12u(uint32_t p)
+{
+    arr2u v;
+    v.x() = hash11u(p);
+    v.y() = hash11u(p + 4200861443);
+    return v;
+}
+
 inline arr2u hash22u(arr2u v)
 {
     // pcg2d
@@ -33,25 +53,3 @@ inline arr2u hash22u(arr2u v)
 }
 
 inline arr2 hash22f(arr2u v) { return convert_u32_f01<2>(hash22u(v)); }
-
-inline uint32_t xxhash32(uint32_t p)
-{
-    const uint32_t PRIME32_2 = 2246822519U, PRIME32_3 = 3266489917U;
-    const uint32_t PRIME32_4 = 668265263U, PRIME32_5 = 374761393U;
-    uint32_t h32 = p + PRIME32_5;
-    h32 = PRIME32_4 * ((h32 << 17) | (h32 >> (32 - 17)));
-    h32 = PRIME32_2 * (h32 ^ (h32 >> 15));
-    h32 = PRIME32_3 * (h32 ^ (h32 >> 13));
-    return h32 ^ (h32 >> 16);
-}
-
-inline uint32_t xxhash32(uint32_t x, uint32_t y)
-{
-    const uint32_t PRIME32_2 = 2246822519U, PRIME32_3 = 3266489917U;
-    const uint32_t PRIME32_4 = 668265263U, PRIME32_5 = 374761393U;
-    uint32_t h32 = y + PRIME32_5 + x * PRIME32_3;
-    h32 = PRIME32_4 * ((h32 << 17) | (h32 >> (32 - 17)));
-    h32 = PRIME32_2 * (h32 ^ (h32 >> 15));
-    h32 = PRIME32_3 * (h32 ^ (h32 >> 13));
-    return h32 ^ (h32 >> 16);
-}
