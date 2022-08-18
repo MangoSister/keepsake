576f061079e4ba15d0629f56667edbb4daca1437
Yang Zhou <zhouyang@fb.com>
2022-08-17 18:50:31 -0700

    Added colormaps.

---
+++ colormap.h
@@ -0,0 +1,10 @@
+#pragma once
+#include "maths.h"
+
+namespace colormap
+{
+constexpr int plasma_size = 256;
+extern const color3 plasma[plasma_size];
+constexpr int viridis_size = 256;
+extern const color3 viridis[viridis_size];
+} // namespace colormap
