9d18e4727dfce99ecbbec3a2674e549001026a0e
MangoSister <yyp0502@gmail.com>
2023-11-13 13:34:15 -0800

    read/write rgba16f/32f surfaces.

--- cuda/texture_device.cuh
+++ cuda/texture_device.cuh
@@ -7,7 +7,18 @@
 namespace ksc
 {
 
-__device__ inline void write_to_half_render_target(const Surface2D &rt, ksc::vec2i pixel, ksc::color3 color)
+__device__ inline void write_to_render_target_rgbaf32(const Surface2D &rt, ksc::vec2i pixel, ksc::color3 color)
+{
+    size_t texel_bytes = (rt.format_desc.x + rt.format_desc.y + rt.format_desc.z + rt.format_desc.w) / 8;
+    float4 rgba;
+    rgba.x = color.x;
+    rgba.y = color.y;
+    rgba.z = color.z;
+    rgba.w = 1.0;
+    surf2Dwrite(rgba, rt.surf_obj, pixel.x * texel_bytes, pixel.y);
+}
+
+__device__ inline void write_to_render_target_rgbaf16(const Surface2D &rt, ksc::vec2i pixel, ksc::color3 color)
 {
     size_t texel_bytes = (rt.format_desc.x + rt.format_desc.y + rt.format_desc.z + rt.format_desc.w) / 8;
     ushort4 rgba;
@@ -18,7 +29,18 @@ __device__ inline void write_to_half_render_target(const Surface2D &rt, ksc::vec
     surf2Dwrite(rgba, rt.surf_obj, pixel.x * texel_bytes, pixel.y);
 }
 
-__device__ inline ksc::color3 read_from_half_render_target(const Surface2D &rt, ksc::vec2i pixel)
+__device__ inline ksc::color3 read_from_render_target_rgbaf32(const Surface2D &rt, ksc::vec2i pixel)
+{
+    size_t texel_bytes = (rt.format_desc.x + rt.format_desc.y + rt.format_desc.z + rt.format_desc.w) / 8;
+    float4 f32 = surf2Dread<float4>(rt.surf_obj, pixel.x * texel_bytes, pixel.y);
+    color3 rgb;
+    rgb.x = f32.x;
+    rgb.y = f32.y;
+    rgb.z = f32.z;
+    return rgb;
+}
+
+__device__ inline ksc::color3 read_from_render_target_rgbaf16(const Surface2D &rt, ksc::vec2i pixel)
 {
     size_t texel_bytes = (rt.format_desc.x + rt.format_desc.y + rt.format_desc.z + rt.format_desc.w) / 8;
     ushort4 u16 = surf2Dread<ushort4>(rt.surf_obj, pixel.x * texel_bytes, pixel.y);
