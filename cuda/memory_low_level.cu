6e81e4b0be32ac47c2a40c30ed6cd4c033ac1d83
MangoSister <yyp0502@gmail.com>
2023-12-31 12:26:35 -0800

    misc

--- cuda/memory_low_level.cu
+++ cuda/memory_low_level.cu
@@ -25,79 +25,79 @@ void set_default_security_descriptor(CUmemAllocationProp *prop)
     return;
 #elif defined(WIN32) || defined(_WIN32) || defined(WIN64) || defined(_WIN64)
     static const char sddl[] = "D:P(OA;;GARCSDWDWOCCDCLCSWLODTWPRPCRFA;;;WD)";
-    static OBJECT_ATTRIBUTES objAttributes;
-    static bool objAttributesConfigured = false;
+    static OBJECT_ATTRIBUTES obj_attributes;
+    static bool obj_attributes_configured = false;
 
-    if (!objAttributesConfigured) {
-        PSECURITY_DESCRIPTOR secDesc;
-        BOOL result = ConvertStringSecurityDescriptorToSecurityDescriptorA(sddl, SDDL_REVISION_1, &secDesc, NULL);
+    if (!obj_attributes_configured) {
+        PSECURITY_DESCRIPTOR sec_desc;
+        BOOL result = ConvertStringSecurityDescriptorToSecurityDescriptorA(sddl, SDDL_REVISION_1, &sec_desc, NULL);
         if (result == 0) {
             fprintf(stderr, "IPC failure: getDefaultSecurityDescriptor Failed! (%d)\n", GetLastError());
         }
 
-        InitializeObjectAttributes(&objAttributes, NULL, 0, NULL, secDesc);
+        InitializeObjectAttributes(&obj_attributes, NULL, 0, NULL, sec_desc);
 
-        objAttributesConfigured = true;
+        obj_attributes_configured = true;
     }
 
-    prop->win32HandleMetaData = &objAttributes;
+    prop->win32HandleMetaData = &obj_attributes;
     return;
 #endif
 }
 
 CudaShareableLowLevelMemory cuda_alloc_device_low_level(size_t size, int device)
 {
-    // `ipcHandleTypeFlag` specifies the platform specific handle type this sample
+    // `ipc_handle_type_flag` specifies the platform specific handle type this sample
     // uses for importing and exporting memory allocation. On Linux this sample
     // specifies the type as CU_MEM_HANDLE_TYPE_POSIX_FILE_DESCRIPTOR meaning that
     // file descriptors will be used. On Windows this sample specifies the type as
     // CU_MEM_HANDLE_TYPE_WIN32 meaning that NT HANDLEs will be used. The
-    // ipcHandleTypeFlag variable is a convenience variable and is passed by value
+    // ipc_handle_type_flag variable is a convenience variable and is passed by value
     // to individual requests.
 #if defined(__linux__)
-    CUmemAllocationHandleType ipcHandleTypeFlag = CU_MEM_HANDLE_TYPE_POSIX_FILE_DESCRIPTOR;
+    CUmemAllocationHandleType ipc_handle_type_flag = CU_MEM_HANDLE_TYPE_POSIX_FILE_DESCRIPTOR;
 #else
-    CUmemAllocationHandleType ipcHandleTypeFlag = CU_MEM_HANDLE_TYPE_WIN32;
+    CUmemAllocationHandleType ipc_handle_type_flag = CU_MEM_HANDLE_TYPE_WIN32;
 #endif
 
-    CUmemAllocationProp allocProp = {};
-    allocProp.type = CU_MEM_ALLOCATION_TYPE_PINNED;
-    allocProp.location.type = CU_MEM_LOCATION_TYPE_DEVICE;
-    allocProp.location.id = device;
-    allocProp.win32HandleMetaData = nullptr;
-    allocProp.requestedHandleTypes = CU_MEM_HANDLE_TYPE_WIN32;
+    CUmemAllocationProp alloc_prop = {};
+    alloc_prop.type = CU_MEM_ALLOCATION_TYPE_PINNED;
+    alloc_prop.location.type = CU_MEM_LOCATION_TYPE_DEVICE;
+    alloc_prop.location.id = device;
+    alloc_prop.win32HandleMetaData = nullptr;
+    alloc_prop.requestedHandleTypes = CU_MEM_HANDLE_TYPE_WIN32;
 
     // Windows-specific LPSECURITYATTRIBUTES is required when
     // CU_MEM_HANDLE_TYPE_WIN32 is used. The security attribute defines the scope
     // of which exported allocations may be tranferred to other processes. For all
     // other handle types, pass NULL.
-    set_default_security_descriptor(&allocProp);
+    set_default_security_descriptor(&alloc_prop);
 
     // Get the recommended granularity for m_cudaDevice.
     size_t granularity = 0;
-    CU_CHECK(cuMemGetAllocationGranularity(&granularity, &allocProp, CU_MEM_ALLOC_GRANULARITY_RECOMMENDED));
+    CU_CHECK(cuMemGetAllocationGranularity(&granularity, &alloc_prop, CU_MEM_ALLOC_GRANULARITY_RECOMMENDED));
 
     size_t size_rounded = (((size + granularity - 1) / granularity) * granularity);
 
     // Reserve the required contiguous VA space for the allocations
-    CUdeviceptr d_ptr = 0;
-    CU_CHECK(cuMemAddressReserve(&d_ptr, size_rounded, granularity, 0U, 0));
+    CUdeviceptr dptr = 0;
+    CU_CHECK(cuMemAddressReserve(&dptr, size_rounded, granularity, 0U, 0));
 
     // Create the allocations as a pinned allocation on this device.
     // Create an allocation to store all the positions of points on the xy plane
     // and a second allocation which stores information if the corresponding
     // position is inside the unit circle or not.
     CUmemGenericAllocationHandle handle{};
-    CU_CHECK(cuMemCreate(&handle, size_rounded, &allocProp, 0));
+    CU_CHECK(cuMemCreate(&handle, size_rounded, &alloc_prop, 0));
 
     // Export the allocation to a platform-specific handle. The type of handle
     // requested here must match the requestedHandleTypes field in the prop
     // structure passed to cuMemCreate. The handle obtained here will be passed to
     // vulkan to import the allocation.
     ShareableHandle shareable_handle{};
-    CU_CHECK(cuMemExportToShareableHandle((void *)&shareable_handle, handle, ipcHandleTypeFlag, 0));
+    CU_CHECK(cuMemExportToShareableHandle((void *)&shareable_handle, handle, ipc_handle_type_flag, 0));
 
-    CU_CHECK(cuMemMap(d_ptr, size_rounded, 0, handle, 0));
+    CU_CHECK(cuMemMap(dptr, size_rounded, 0, handle, 0));
 
     // Release the handles for the allocation. Since the allocation is currently
     // mapped to a VA range with a previous call to cuMemMap the actual freeing of
@@ -105,16 +105,16 @@ CudaShareableLowLevelMemory cuda_alloc_device_low_level(size_t size, int device)
     // allocation will be kept live until it is unmapped.
     CU_CHECK(cuMemRelease(handle));
 
-    CUmemAccessDesc accessDescriptor = {};
-    accessDescriptor.location.id = device;
-    accessDescriptor.location.type = CU_MEM_LOCATION_TYPE_DEVICE;
-    accessDescriptor.flags = CU_MEM_ACCESS_FLAGS_PROT_READWRITE;
+    CUmemAccessDesc access_descriptor = {};
+    access_descriptor.location.id = device;
+    access_descriptor.location.type = CU_MEM_LOCATION_TYPE_DEVICE;
+    access_descriptor.flags = CU_MEM_ACCESS_FLAGS_PROT_READWRITE;
 
     // Apply the access descriptor to the whole VA range. Essentially enables
     // Read-Write access to the range.
-    CU_CHECK(cuMemSetAccess(d_ptr, size_rounded, &accessDescriptor, 1));
+    CU_CHECK(cuMemSetAccess(dptr, size_rounded, &access_descriptor, 1));
 
-    return CudaShareableLowLevelMemory{.dptr = d_ptr, .shareable_handle = shareable_handle, .size = size_rounded};
+    return CudaShareableLowLevelMemory{.dptr = dptr, .shareable_handle = shareable_handle, .size = size_rounded};
 }
 
 void cuda_free_device_low_level(const CudaShareableLowLevelMemory &m)
