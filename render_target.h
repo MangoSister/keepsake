7a7f20f56addfd18427971e310aed55516104e0d
MangoSister <yyp0502@gmail.com>
2024-04-28 00:43:59 -0700

    Render target parallel pixelwise operation.

--- render_target.h
+++ render_target.h
@@ -1,6 +1,7 @@
 #pragma once
 #include "assertion.h"
 #include "maths.h"
+#include "parallel.h"
 #include <filesystem>
 #include <vector>
 namespace fs = std::filesystem;
@@ -19,21 +20,34 @@ struct RenderTarget
     color3 &operator()(int x, int y) { return pixels[y * width + x]; }
     const color3 &operator()(int x, int y) const { return pixels[y * width + x]; }
 
+    template <bool parallel = false>
     RenderTarget &operator+=(const RenderTarget &other)
     {
         ASSERT(width == other.width && height == other.height);
-        for (int i = 0; i < (int)pixels.size(); ++i) {
-            pixels[i] += other.pixels[i];
-        }
+
+        pixelwise_op<parallel>([&](int index, color3 pixel) { return pixel + other.pixels[index]; });
+
         return *this;
     }
 
+    template <bool parallel = false>
     RenderTarget &operator*=(const color3 &scalar)
     {
-        for (int i = 0; i < (int)pixels.size(); ++i) {
-            pixels[i] = pixels[i].cwiseProduct(scalar);
-        }
+        pixelwise_op<parallel>([&](int index, color3 pixel) { return pixel.cwiseProduct(scalar); });
+
         return *this;
+    }
+
+    template <bool parallel, typename Op>
+    void pixelwise_op(const Op &op)
+    {
+        if constexpr (parallel) {
+            parallel_for((int)pixels.size(), [&](int i) { pixels[i] = op(i, pixels[i]); });
+        } else {
+            for (int i = 0; i < (int)pixels.size(); ++i) {
+                pixels[i] = op(i, pixels[i]);
+            }
+        }
     }
 
     void save_to_png(const fs::path &path) const;
