4414cd141e377ae67847df8428fcf8ed42473d67
MangoSister <yyp0502@gmail.com>
2024-08-04 14:48:37 -0700

    Sample 64 bit mask.

--- rng.h
+++ rng.h
@@ -456,4 +456,58 @@ inline ks::quat sample_uniform_rotation(float u, float v, float w)
     return q;
 }
 
+// https://graphics.stanford.edu/~seander/bithacks.html#SelectPosFromMSBRank
+// https://yduf.github.io/bit-select-random-set/
+// (counting from the left)
+inline uint32_t sample_bitmask_64(uint64_t v, float u)
+{
+    // Input value to find position with rank r.
+    int bitcnt = std::popcount(v);
+    // unsigned int r;      // Input: bit's desired rank [1-64].
+    unsigned int r = std::clamp((int)std::floor(u * bitcnt), 0, bitcnt - 1) + 1;
+    unsigned int s;      // Output: Resulting position of bit with rank r [1-64]
+    uint64_t a, b, c, d; // Intermediate temporaries for bit count.
+    unsigned int t;      // Bit count temporary.
+
+    // Do a normal parallel bit count for a 64-bit integer,
+    // but store all intermediate steps.
+    // a = (v & 0x5555...) + ((v >> 1) & 0x5555...);
+    a = v - ((v >> 1) & ~0UL / 3);
+    // b = (a & 0x3333...) + ((a >> 2) & 0x3333...);
+    b = (a & ~0UL / 5) + ((a >> 2) & ~0UL / 5);
+    // c = (b & 0x0f0f...) + ((b >> 4) & 0x0f0f...);
+    c = (b + (b >> 4)) & ~0UL / 0x11;
+    // d = (c & 0x00ff...) + ((c >> 8) & 0x00ff...);
+    d = (c + (c >> 8)) & ~0UL / 0x101;
+    t = (d >> 32) + (d >> 48);
+    // Now do branchless select!
+    s = 64;
+    // if (r > t) {s -= 32; r -= t;}
+    s -= ((t - r) & 256) >> 3;
+    r -= (t & ((t - r) >> 8));
+    t = (d >> (s - 16)) & 0xff;
+    // if (r > t) {s -= 16; r -= t;}
+    s -= ((t - r) & 256) >> 4;
+    r -= (t & ((t - r) >> 8));
+    t = (c >> (s - 8)) & 0xf;
+    // if (r > t) {s -= 8; r -= t;}
+    s -= ((t - r) & 256) >> 5;
+    r -= (t & ((t - r) >> 8));
+    t = (b >> (s - 4)) & 0x7;
+    // if (r > t) {s -= 4; r -= t;}
+    s -= ((t - r) & 256) >> 6;
+    r -= (t & ((t - r) >> 8));
+    t = (a >> (s - 2)) & 0x3;
+    // if (r > t) {s -= 2; r -= t;}
+    s -= ((t - r) & 256) >> 7;
+    r -= (t & ((t - r) >> 8));
+    t = (v >> (s - 1)) & 0x1;
+    // if (r > t) s--;
+    s -= ((t - r) & 256) >> 8;
+    s = 65 - s;
+
+    // Modified to return bit pos [0-63].
+    return s - 1;
+}
+
 } // namespace ks
